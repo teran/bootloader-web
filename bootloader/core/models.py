056aeefdffe7717516f51e170f4939330ea06f3c
Igor Shishkin <me@teran.ru>
2017-03-18 09:31:41 -0700

    Link methods added to server model
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- bootloader/core/models.py
+++ bootloader/core/models.py
@@ -28,3 +28,12 @@ class Server(models.Model):
     ipmi_password = models.CharField(max_length=255, null=True)
     ipmi_browser_proto = models.IntegerField(
         choices=IPMI_BROWSER_PROTO_CHOICES, null=True)
+
+    def link_webui(self):
+        return '/server/%s/%s.html' % (self.pk, self.fqdn)
+
+    def link_webui_edit(self):
+        return '/server/%s/%s/edit.html' % (self.pk, self.fqdn)
+
+    def link_api(self):
+        return '/api/servers/%s' % (self.pk)
