03e1d84776c2837b10ec8f7967359a2d83f8e2d2
Igor Shishkin <me@teran.ru>
2017-03-21 23:18:20 +0300

    servers moved to new universal controls method
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- bootloader/static/js/bootloader/controls.js
+++ bootloader/static/js/bootloader/controls.js
@@ -25,32 +25,6 @@ $('document').ready(function() {
           }
       }
   });
-  $(".delete-object").click(function() {
-    var objectID = $(this).attr("object-id");
-    var objectName = $(this).attr("object-name");
-    var objectType = $(this).attr("object-type");
-    var postActionURL = $("#post-action-url").val();
-
-    $("#formModalTitle").html("Sure?");
-    $("#formModalBody").html("Are you sure you really want to delete "+objectType+" "+objectName+"?");
-    $("#formModalSubmitButton").addClass("btn-danger btn-default");
-    $("#formModalSubmitButton").html("Delete");
-    $("#formModal").modal('show');
-    $("#formModalSubmitButton").click(function() {
-      $.ajax({
-        url: '/api/'+objectType+'s/'+objectID+"/",
-        type: 'DELETE',
-        success: function(result) {
-          if(postActionURL) {
-            $(location).attr("href", postActionURL);
-          }
-        },
-        error: function(result, status, error) {
-          alert("Error performing an action: "+status)
-        }
-      });
-    });
-  });
   $(".object-action-button").click(function() {
     var objectID = $(this).attr("object-id");
     var objectName = $(this).attr("object-name");
@@ -59,6 +33,10 @@ $('document').ready(function() {
     var postActionURL = $(this).attr("post-action-url");
     var actionMethod = $(this).attr("action-method");
     var actionData = $(this).attr("action-data");
+
+    if (!actionData) {
+      actionData = "{}"
+    }
 
     $("#formModalTitle").html("Sure?");
     $("#formModalBody").html("Are you sure you really want to "+objectAction+" "+objectType+" "+objectName+"?");
