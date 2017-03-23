3fcda270e8f5f0b1df8d9b04a26498cdb55885e9
Igor Shishkin <me@teran.ru>
2017-03-23 07:11:18 +0300

    Deployment stub app added
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- bootloader/bootloader/settings.py
+++ bootloader/bootloader/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'django_gravatar_tags',
 
+    'deployments',
     'servers',
     'users',
 ]
---
+++ bootloader/deployments/models.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from servers.models import Server
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.__str__()
+
+class Deployment(models.Model):
+    server = models.ForeignKey(Server, related_name='deployments')
+    profile = models.ForeignKey(Profile, related_name='deployments')
+
+    def __str__(self):
+        return '%s@%s' % (self.profile, self.server)
+
+    def __unicode__(self):
+        return self.__str__()
