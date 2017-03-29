d054b33e207696e617d5014735a106d7ffc71da0
Igor Shishkin <me@teran.ru>
2017-03-29 23:00:29 +0300

    Alphabetical sort

--- bootloader/servers/urls.py
+++ bootloader/servers/urls.py
@@ -4,8 +4,8 @@ import servers.views
 
 urlpatterns = [
     url(r'^$', servers.views.index),
-    url(r'^locations\.html$', servers.views.locations),
     url(r'^index\.html$', servers.views.index),
+    url(r'^locations\.html$', servers.views.locations),
     url(
         r'^(?P<pk>[0-9]{1,11})/(?P<fqdn>[a-z0-9\.-]{1,255}).html$',
         servers.views.server),
