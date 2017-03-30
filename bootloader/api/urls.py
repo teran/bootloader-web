efac4dbd042ca2063bf17b2874a792de1c53917c
Igor Shishkin <me@teran.ru>
2017-03-30 07:35:40 +0300

    Alphabetical sort for routers

--- bootloader/api/urls.py
+++ bootloader/api/urls.py
@@ -8,9 +8,9 @@ router = routers.DefaultRouter()
 router.register(r'files', views.FileViewSet)
 router.register(r'interfaces', views.InterfaceViewSet)
 router.register(r'locations', views.LocationViewSet)
+router.register(r'profiles', views.ProfileViewSet)
 router.register(r'servers', views.ServerViewSet)
 router.register(r'users', views.UserViewSet)
-router.register(r'profiles', views.ProfileViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
