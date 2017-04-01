24279c6610e601f764e5c6df2bebabcbf4b3ffd0
Igor Shishkin <me@teran.ru>
2017-04-01 13:57:04 +0300

    Test tasks to use with celery

---
+++ bootloader/deployments/tasks.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from celery import Celery
+
+app = Celery('tasks')
+app.conf.update(**settings.CELERY_SETTINGS)
+
+@app.task
+def deployment_start(deployment, profile, version, token):
+    import time
+    time.sleep(30)
+    print 'OK'
+
+@app.task
+def deploy_pxe_files(server):
+    import requests
+    print 'OK'
