51fed1f0ba900fdeec31eef82976d6a6bb81e967
Igor Shishkin <me@teran.ru>
2017-04-02 11:02:27 +0300

    Stubbing tasks
    Normally from that side it's not neccessary to have anything in code,
    so leaving interfaces only for tasks registering.

--- bootloader/deployments/tasks.py
+++ bootloader/deployments/tasks.py
@@ -8,11 +8,9 @@ app.conf.update(**settings.CELERY_SETTINGS)
 
 @app.task
 def deployment_start(deployment, profile, version, token):
-    import time
-    time.sleep(30)
-    print 'OK'
+    pass
 
 
 @app.task
 def deploy_pxe_files(server):
-    print 'OK'
+    pass
