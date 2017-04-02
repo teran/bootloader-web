b61df61f87d0462660a246fedf5f523a9f7c8520
Igor Shishkin <me@teran.ru>
2017-04-02 10:53:44 +0300

    More strict validation for request data on export

--- bootloader/export/views.py
+++ bootloader/export/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import Context, Template
 
@@ -6,13 +6,21 @@ from deployments.models import Deployment
 
 
 def file(request, deployment, token, profile, version, file):
-    deployment = get_object_or_404(Deployment, pk=deployment, token=token)
+    deployment = get_object_or_404(
+        Deployment,
+        pk=deployment,
+        token=token,
+        profile__name=profile,
+        profile__version=version)
     profile = deployment.profile
 
     contents = None
     for f in profile.profile.get('files'):
         if f.get('name') == file:
             contents = f.get('contents')
+
+    if contents is None:
+        raise Http404
 
     context = Context({
         'profile': deployment.profile,
