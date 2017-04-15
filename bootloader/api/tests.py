be47a30c0e2fa12062711071c17719a0202488ae
Igor Shishkin <me@teran.ru>
2017-04-15 11:09:24 +0300

    Some trivial tests added for API and auth check to user module

--- bootloader/api/tests.py
+++ bootloader/api/tests.py
@@ -1,1 +1,50 @@
-# Create your tests here.
+from __future__ import unicode_literals
+
+from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth.models import User
+
+
+class APIv1alpha1TestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email='testuser@example.org',
+            password='testpassword')
+        self.token = Token.objects.create(user=self.user)
+
+        self.basic_auth_client = APIClient()
+        self.basic_auth_client.login(
+            username='testuser', password='testpassword')
+
+        self.token_auth_client = APIClient()
+        self.token_auth_client.credentials(
+            HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+        self.handlers = [
+            'deployments',
+            'interfaces',
+            'locations',
+            'profiles',
+            'servers',
+            'users',
+        ]
+
+    def test_basic_auth(self):
+        result = self.basic_auth_client.get(
+            '/api/v1alpha1/', format='json').status_code
+
+        self.assertEqual(result, 200)
+
+    def test_token_auth(self):
+        result = self.token_auth_client.get(
+            '/api/v1alpha1/', format='json').status_code
+
+        self.assertEqual(result, 200)
+
+    def test_api_exports(self):
+        result = self.token_auth_client.get(
+            '/api/v1alpha1/', format='json').json()
+
+        self.assertEqual(sorted(result.keys()), sorted(self.handlers))
--- bootloader/users/tests.py
+++ bootloader/users/tests.py
@@ -7,16 +7,19 @@ class TestUsersAndAuthentication(TestCase):
 
         self.urls = {
             '/': 302,
-            '/servers/index.html': 302,
-            '/servers/locations.html': 302,
+            '/api/': 404,
+            '/api/v1alpha1/': 401,
             '/deployments/deployments.html': 302,
             '/deployments/profiles.html': 302,
+            '/servers/index.html': 302,
+            '/servers/locations.html': 302,
+            '/tools/yaml2json': 200,
             '/user/events.html': 302,
-            '/user/tokens.html': 302,
-            '/user/profile.html': 302,
-            '/user/logout.html': 302,
             '/user/login.html': 200,
+            '/user/logout.html': 302,
+            '/user/profile.html': 302,
             '/user/register.html': 200,
+            '/user/tokens.html': 302,
         }
 
     def test_unauthorized_access(self):
