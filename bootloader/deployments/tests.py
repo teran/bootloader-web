f77059f39e007850ba39cf30b86078723402308b
Igor Shishkin <me@teran.ru>
2017-04-15 20:20:22 +0300

    Simple unit test added to Profile model

--- bootloader/deployments/tests.py
+++ bootloader/deployments/tests.py
@@ -1,1 +1,22 @@
-# Create your tests here.
+import json
+
+from django.test import TestCase
+
+from deployments.models import Profile
+
+
+class ProfileObjectTestCase(TestCase):
+    def setUp(self):
+        self.profile = {
+            'kind': 'Profile',
+            'name': 'testprofile',
+            'version': '0.1'
+        }
+
+    def test_profile_create(self):
+        profile = Profile.objects.create(
+            name='testprofile',
+            version='0.1',
+            profile=json.dumps(self.profile))
+
+        self.assertDictEqual(json.loads(profile.profile), self.profile)
