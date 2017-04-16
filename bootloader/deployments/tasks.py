4fa32e377a0629491607f69fc39292b04f4a1b05
Igor Shishkin <me@teran.ru>
2017-04-16 13:32:58 +0300

    Tasks updated to contain details about new profile scheme

--- bootloader/deployments/tasks.py
+++ bootloader/deployments/tasks.py
@@ -2,12 +2,36 @@ from django.conf import settings
 
 from celery import Celery
 
+
 app = Celery('tasks')
 app.conf.update(**settings.CELERY_SETTINGS)
 
 
 @app.task
-def deployment_created(deployment):
-    d = Deployment.objects.get(pk=deployment)
+def deployment_created(*args, **kwargs):
+    pass
 
-    d.profile.profile.get('workflow').get('new')
+
+@app.task
+def evaluate_new(*args, **kwargs):
+    pass
+
+
+@app.task
+def evaluate_preparing(*args, **kwargs):
+    pass
+
+
+@app.task
+def evaluate_installing(*args, **kwargs):
+    pass
+
+
+@app.task
+def evaluate_configuring(*args, **kwargs):
+    pass
+
+
+@app.task
+def evaluate_postconfiguring(*args, **kwargs):
+    pass
