62fa20f5e606023440813fd60eb35f87f34d5969
Igor Shishkin <me@teran.ru>
2017-04-22 20:57:59 +0300

    urls route updated for proper version format
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- bootloader/export/urls.py
+++ bootloader/export/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from export import views
 
 urlpatterns = [
-    url(r'^file\/(?P<deployment>[0-9]+)\/(?P<token>[a-f0-9]{64})\/(?P<profile>[a-zA-Z0-9\._-]+)\/(?P<version>[0-9\.]+)\/(?P<file>[a-zA-Z0-9\._-]+)$', views.file),  # noqa
+    url(r'^file\/(?P<deployment>[0-9]+)\/(?P<token>[a-f0-9]{64})\/(?P<profile>[a-zA-Z0-9\._-]+)\/(?P<version>[a-zA-Z0-9\.-]+)\/(?P<file>[a-zA-Z0-9\._-]+)$', views.file),  # noqa
 ]
