af9dbc924190448ff5a3d55c6c6ef096261bd882
Igor Shishkin <me@teran.ru>
2017-04-23 11:13:52 +0300

    create-deployment and delete-deployment example scripts added
    Signed-off-by: Igor Shishkin <me@teran.ru>

---
+++ examples/scripts/create-deployment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import requests
+import sys
+
+try:
+    API_TOKEN = os.environ['API_TOKEN']
+    BOOTLOADER_URL = os.environ['BOOTLOADER_URL']
+    SERVER_NAME = os.environ['SERVER_NAME']
+    PROFILE_ID = os.environ['PROFILE_ID']
+except KeyError as e:
+    print("""
+    API_TOKEN and BOOTLOADER_URL must be passed as environment variables
+
+    SERVER_NAME and PROFILE_ID are used as a payload for API
+
+    Example:
+    export API_TOKEN=<token>
+    export BOOTLOADER_URL=<bootloaderurl>
+    export SERVER_NAME=<hostname>
+    export PROFILE_ID=<profile>
+    %s
+    """ % __file__)
+    sys.exit(1)
+
+headers = {
+    'User-Agent': 'Bootloader Create Deployment example script/0.1',
+    'Authorization': 'Token %s' % (API_TOKEN,),
+}
+
+r = requests.post(
+    '%s/api/v1alpha1/deployments/' % (BOOTLOADER_URL,),
+    headers=headers,
+    data={
+        'server': SERVER_NAME,
+        'profile': PROFILE_ID,
+    })
+
+print(r.content)
---
+++ examples/scripts/delete-deployments.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import requests
+import sys
+
+try:
+    API_TOKEN = os.environ['API_TOKEN']
+    BOOTLOADER_URL = os.environ['BOOTLOADER_URL']
+except KeyError as e:
+    print("""
+    API_TOKEN and BOOTLOADER_URL must be passed as environment variables
+
+    Example:
+    export API_TOKEN=<token>
+    export BOOTLOADER_URL=<bootloaderurl>
+    %s
+    """ % __file__)
+    sys.exit(1)
+
+
+headers = {
+    'User-Agent': 'Bootloader Delete Deployment example script/0.1',
+    'Authorization': 'Token %s' % (API_TOKEN,),
+}
+
+r = requests.get(
+    '%s/api/v1alpha1/deployments/' % BOOTLOADER_URL,
+    headers=headers).json()
+
+for deployment in r:
+    requests.delete(
+        '%s/api/v1alpha1/deployments/%s/' % (
+            BOOTLOADER_URL, deployment['pk'],),
+        headers=headers)
