f30039cb43d998baf212737c191a8b0865e3a44a
Igor Shishkin <me@teran.ru>
2017-04-30 03:58:59 +0300

    Fixed the issue in API allows to reveal data
    In custom permission class was an issue of not checking if user is
    authenticated. The issue is fixed and covered with tests.

--- bootloader/api/tests.py
+++ bootloader/api/tests.py
@@ -34,23 +34,57 @@ class APIv1alpha1TestCase(APITestCase):
             'users',
         ]
 
-    def test_noauth(self):
-        result = self.noauth_client.get(
-            '/api/v1alpha1/', format='json').status_code
+        self.api_versions = [
+            'v1alpha1',
+        ]
 
-        self.assertEqual(result, 401)
+        self.urls_noauth = {}
+        self.urls_auth = {}
+
+        for version in self.api_versions:
+            for handler in self.handlers:
+                if version not in self.urls_noauth:
+                    self.urls_noauth[version] = {}
+                if version not in self.urls_auth:
+                    self.urls_auth[version] = {}
+                self.urls_auth[version][handler] = 200
+                self.urls_noauth[version][handler] = 401
+
+    def test_noauth(self):
+        result = {}
+        for version in self.api_versions:
+            for handler in self.handlers:
+                if version not in result:
+                    result[version] = {}
+                result[version][handler] = self.noauth_client.get(
+                    '/api/%s/%s/' % (version, handler,),
+                    format='json').status_code
+
+        self.assertEqual(result, self.urls_noauth)
 
     def test_basic_auth(self):
-        result = self.basic_auth_client.get(
-            '/api/v1alpha1/', format='json').status_code
+        result = {}
+        for version in self.api_versions:
+            for handler in self.handlers:
+                if version not in result:
+                    result[version] = {}
+                result[version][handler] = self.basic_auth_client.get(
+                    '/api/%s/%s/' % (version, handler,),
+                    format='json').status_code
 
-        self.assertEqual(result, 200)
+        self.assertEqual(result, self.urls_auth)
 
     def test_token_auth(self):
-        result = self.token_auth_client.get(
-            '/api/v1alpha1/', format='json').status_code
+        result = {}
+        for version in self.api_versions:
+            for handler in self.handlers:
+                if version not in result:
+                    result[version] = {}
+                result[version][handler] = self.token_auth_client.get(
+                    '/api/%s/%s/' % (version, handler,),
+                    format='json').status_code
 
-        self.assertEqual(result, 200)
+        self.assertEqual(result, self.urls_auth)
 
     def test_api_exports(self):
         result = self.token_auth_client.get(
--- bootloader/tools/api/permissions.py
+++ bootloader/tools/api/permissions.py
@@ -4,8 +4,8 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class StaffOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_staff or \
-            request.method in SAFE_METHODS
+            (request.method in SAFE_METHODS and request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or \
-            request.method in SAFE_METHODS
+            (request.method in SAFE_METHODS and request.user.is_authenticated)
